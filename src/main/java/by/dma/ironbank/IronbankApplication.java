70bac64bf31040fdb3aeae9ab1424a39662d7ef9
Dzmitry Marudau <vinmaster@tut.by>
2020-12-20 00:51:44 +0300

    Amount of beans in context

--- src/main/java/by/dma/ironbank/IronbankApplication.java
+++ src/main/java/by/dma/ironbank/IronbankApplication.java
@@ -3,6 +3,7 @@ package by.dma.ironbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import by.dma.ironbank.model.ProphetProperties;
 
@@ -11,7 +12,10 @@ import by.dma.ironbank.model.ProphetProperties;
 public class IronbankApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(IronbankApplication.class, args);
+    ConfigurableApplicationContext context = SpringApplication.run(IronbankApplication.class, args);
+    System.out.println("####################################");
+    System.out.printf("# Amount of beans in context: %4s #%n", context.getBeanDefinitionCount());
+    System.out.println("####################################");
   }
 
 }
