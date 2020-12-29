8e349f0e5b11e8115c8880589425407e14d8ca7c
Dzmitry Marudau <vinmaster@tut.by>
2020-12-30 00:55:06 +0300

    Avoid useless dependencies

--- src/test/java/by/dma/ironbank/controller/IronBankControllerTest.java
+++ src/test/java/by/dma/ironbank/controller/IronBankControllerTest.java
@@ -1,13 +1,13 @@
 package by.dma.ironbank.controller;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author dzmitry.marudau
  * @since 2020.4
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 public class IronBankControllerTest {
@@ -35,7 +35,7 @@ public class IronBankControllerTest {
   @MockBean
   ProphetService prophetService;
 
-  @Before
+  @BeforeEach
   public void setUp() throws Exception {
     when(prophetService.willSurvive(anyString()))
             .thenReturn(true);
